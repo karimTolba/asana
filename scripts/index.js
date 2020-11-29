dd7ad56b4a893adb64f8753868e6425a05684b7a
karim <tolbaaalove333@gmail.com>
2020-11-29 04:44:01 +0200

    first commit

---
+++ scripts/index.js
@@ -0,0 +1,16 @@
+function fas_hide(){
+
+    const aside = document.getElementById("aside");
+
+    aside.style.animation = "hide 3s forwards";
+
+}
+
+function fas_show(){
+
+    const aside = document.getElementById("aside");
+
+    aside.style.animation = "show 3s forwards";
+
+}
+
